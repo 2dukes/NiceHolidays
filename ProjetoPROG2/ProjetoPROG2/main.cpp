4fe189109d22d62261c39aba03ba48922b45adbd
WhiteLizard <ruipinto02@hotmail.com>
2019-05-02 12:19:12 +0100

    Classes Sketch
    ...

---
+++ ProjetoPROG2/ProjetoPROG2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Agency.h"
+#include "Menus.h"
+// #include "utils.h"
+
+
+int main() {
+	cout << "Hi,..." << endl;
+	system("pause");
+	//Agency agency(AGENCY_FILE_NAME);   // create the agency
+
+	//mainMenu(agency); // initial menu inicial with the major options of the application
+
+	return 0;
+}
